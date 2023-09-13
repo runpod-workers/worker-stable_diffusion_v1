66f4199e86a7299a9bbcc82f3fbe63d4d361a480
Justin Merrell <merrelljustin@gmail.com>
2023-09-13 15:00:32 -0400

    feat: defaults to base64

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,6 +1,7 @@
 ''' infer.py for runpod worker '''
 
 import os
+import base64
 import predict
 import argparse
 
@@ -10,6 +11,18 @@ from runpod.serverless.utils.rp_upload import upload_file_to_bucket
 from runpod.serverless.utils import rp_download, rp_cleanup
 
 from rp_schema import INPUT_SCHEMA
+
+
+def upload_or_base64_encode(file_name, img_path):
+    """
+    Uploads image to S3 bucket if it is available, otherwise returns base64 encoded image.
+    """
+    if os.environ.get('BUCKET_ENDPOINT_URL', False):
+        return upload_file_to_bucket(file_name, img_path)
+
+    with open(img_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string.decode("utf-8")
 
 
 def run(job):
@@ -58,10 +71,10 @@ def run(job):
     job_output = []
     for index, img_path in enumerate(img_paths):
         file_name = f"{job['id']}_{index}.png"
-        image_url = upload_file_to_bucket(file_name, img_path)
+        image_return = upload_or_base64_encode(file_name, img_path)
 
         job_output.append({
-            "image": image_url,
+            "image": image_return,
             "seed": validated_input['seed'] + index
         })
 
